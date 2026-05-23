83e2924e77a6555f2f361daa9ab9010dd38b36a7
devkumarkhardia <devkumarkhardia@gmail.com>
2026-05-23 16:15:15 +0530

    Create xor_with_nandgate.v

---
+++ Assignment1/xor_with_nandgate.v
@@ -0,0 +1,9 @@
+timescale 1ns / 1ps
+module xor_with_nandgate(input a, input b, output wire y);
+wire n1,n2,n3,n4;
+nand(n1,a,a);
+nand(n3,n1,b);
+nand(n2,b,b);
+nand(n4,a,n2);
+nand(y,n3,n4);
+endmodule
