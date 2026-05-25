7efdf5652b615612e6b80bf9568a8d25be4a2aef
devkumarkhardia <devkumarkhardia@gmail.com>
2026-05-25 09:50:43 +0530

    Create tb_ThreebitFA_Structural.v

---
+++ Assignment1/tb_ThreebitFA_Structural.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+module testbench;
+reg [2:0] a,b;
+reg cin;
+wire [2:0] sum;
+wire cout;
+
+integer i,j,k;
+threebitadder t1(
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+);
+initial begin
+$monitor("a=%b b=%b cin=%b sum=%b cout=%b",
+          a,b,cin,sum,cout);
+for(i=0;i<8;i=i+1)
+begin
+    for(j=0;j<8;j=j+1)
+    begin
+        for(k=0;k<2;k=k+1)
+        begin
+            a=i;
+            b=j;
+            cin=k;
+            #10;
+        end
+    end
+end
+$finish;
+end
+endmodule
